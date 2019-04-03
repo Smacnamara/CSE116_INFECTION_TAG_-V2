7c35e170c2a343ea192e7c87df8728f1865ec908
sean <smacnamara13@gmail.com>
2019-04-03 15:51:36 -0400

    enemy collision

--- src/View/JS/GameScene.js
+++ src/View/JS/GameScene.js
@@ -23,6 +23,7 @@ class GameScene extends Phaser.Scene{
         this.player = this.physics.add.sprite(150, 400, "livePlayer")
         this.player.setCollideWorldBounds(true);
         this.enemy = this.physics.add.sprite(800, 300, "infected")
+        this.enemy.setCollideWorldBounds(true);
 
         
 
@@ -51,6 +52,8 @@ class GameScene extends Phaser.Scene{
         this.key_W = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W)
         this.key_S = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S)
 
+        this.cursors = this.input.keyboard.createCursorKeys();
+
 
     }
 
@@ -62,6 +65,7 @@ class GameScene extends Phaser.Scene{
         //WRITE GAMESTATE
 
         this.physics.collide(this.player, this.walls)
+        this.physics.collide(this.enemy, this.walls)
         this.physics.overlap(this.player, this.enemy, function(){
             let xpos = this.player.x
             let ypos = this.player.y
@@ -72,6 +76,8 @@ class GameScene extends Phaser.Scene{
 
         this.player.body.velocity.x = 0
         this.player.body.velocity.y = 0
+        this.enemy.body.velocity.x = 0
+        this.enemy.body.velocity.y = 0
         
         if (this.key_A.isDown){
             this.player.body.velocity.x = -150
@@ -84,6 +90,19 @@ class GameScene extends Phaser.Scene{
         }else if (this.key_S.isDown){
             this.player.body.velocity.y = 150;
         }
+
+        if (this.cursors.left.isDown){
+    	    this.enemy.body.velocity.x = -170;
+        }else if (this.cursors.right.isDown){
+    	    this.enemy.body.velocity.x = 170;
+        }
+
+        if (this.cursors.up.isDown){
+    	    this.enemy.body.velocity.y = -170;
+        }else if (this.cursors.down.isDown){
+    	    this.enemy.body.velocity.y = 170;
+        }
+
         //SEND GAME STATE
         
     }
