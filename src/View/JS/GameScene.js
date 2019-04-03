260fa985a98ea05904dadc9e186e34d4592ceee9
sean <smacnamara13@gmail.com>
2019-04-03 04:33:58 -0400

    added movement and wall collision

--- src/View/JS/GameScene.js
+++ src/View/JS/GameScene.js
@@ -20,7 +20,9 @@ class GameScene extends Phaser.Scene{
         
         this.add.sprite(500, 500, "backgroundGame")
         this.walls = this.physics.add.staticGroup();
-        this.player = this.add.sprite(150, 400, "livePlayer")
+        this.player = this.physics.add.sprite(150, 400, "livePlayer")
+        this.player.setCollideWorldBounds(true);
+        
 
         var level = [
             "xxxxxxxxxxxxxxxxxxxxxxxxxx",
@@ -41,10 +43,32 @@ class GameScene extends Phaser.Scene{
                 }
             }
         }
+
+        this.key_A = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A)
+        this.key_D = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D)
+        this.key_W = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W)
+        this.key_S = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S)
+
+
     }
 
     update() {
         this.physics.collide(this.player, this.walls)
+
+        this.player.body.velocity.x = 0
+        this.player.body.velocity.y = 0
+
+        if (this.key_A.isDown){
+            this.player.body.velocity.x = -150
+        }else if (this.key_D.isDown){
+            this.player.body.velocity.x = 150;
+        }
+    
+        if (this.key_W.isDown){
+            this.player.body.velocity.y = -150
+        }else if (this.key_S.isDown){
+            this.player.body.velocity.y = 150;
+        }
     }
 
     
