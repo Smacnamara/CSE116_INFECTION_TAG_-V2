e11da0d6158eb72230ee60cec080dcacb716d22d
sean <smacnamara13@gmail.com>
2019-04-03 06:04:26 -0400

    get fucking tagged bitch

--- src/View/JS/GameScene.js
+++ src/View/JS/GameScene.js
@@ -22,8 +22,7 @@ class GameScene extends Phaser.Scene{
         this.walls = this.physics.add.staticGroup();
         this.player = this.physics.add.sprite(150, 400, "livePlayer")
         this.player.setCollideWorldBounds(true);
-        this.player.body.bounce.y = 1.0
-        this.player.body.bounce.x = 1.0
+        this.enemy = this.physics.add.sprite(800, 300, "infected")
 
         
 
@@ -57,9 +56,15 @@ class GameScene extends Phaser.Scene{
 
     update() {
         this.physics.collide(this.player, this.walls)
+        this.physics.overlap(this.player, this.enemy, function(){
+            let xpos = this.player.x
+            let ypos = this.player.y
+            this.player.destroy()
+            this.player = this.physics.add.sprite(xpos, this.player.y, "infected")
+        },null, this)
 
-        //this.player.body.velocity.x = 0
-        //this.player.body.velocity.y = 0
+        this.player.body.velocity.x = 0
+        this.player.body.velocity.y = 0
 
         if (this.key_A.isDown){
             this.player.body.velocity.x = -150
